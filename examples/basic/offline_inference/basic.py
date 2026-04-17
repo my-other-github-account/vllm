df405302294ba5b1677fe88e4ecd4c466630c6ed
Woosuk Kwon <woosuk@inferact.ai>
2026-04-17 03:23:49 +0000

    profile
    Signed-off-by: Woosuk Kwon <woosuk@inferact.ai>

--- examples/basic/offline_inference/basic.py
+++ examples/basic/offline_inference/basic.py
@@ -16,7 +16,7 @@ sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
 
 def main():
     # Create an LLM.
-    llm = LLM(model="facebook/opt-125m")
+    llm = LLM(model="nvidia/DeepSeek-V3.2-NVFP4", enforce_eager=True, tensor_parallel_size=4, kernel_config={"enable_flashinfer_autotune": False})
     # Generate texts from the prompts.
     # The output is a list of RequestOutput objects
     # that contain the prompt, generated text, and other information.
--- examples/offline_inference/simple_profiling.py
+++ examples/offline_inference/simple_profiling.py
@@ -2,31 +2,46 @@
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
 
 import time
+import os
+
+os.environ["VLLM_USE_SPECIALIZED_MODELS"] = "1"
+os.environ["VLLM_USE_V2_MODEL_RUNNER"] = "1"
 
 from vllm import LLM, SamplingParams
 
 # Sample prompts.
 prompts = [
-    "Hello, my name is",
-    "The president of the United States is",
-    "The capital of France is",
-    "The future of AI is",
+    [0] * 10_000,
+    [1] * 10_000,
+    [2] * 10_000,
+    [3] * 10_000,
+    [4] * 10_000,
+    [5] * 10_000,
+    [6] * 10_000,
+    [7] * 10_000,
 ]
 # Create a sampling params object.
-sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
+sampling_params = SamplingParams(temperature=0.0)
 
 
 def main():
     # Create an LLM.
     llm = LLM(
-        model="facebook/opt-125m",
-        tensor_parallel_size=1,
+        model="nvidia/DeepSeek-V3.2-NVFP4",
+        tensor_parallel_size=4,
+        kernel_config={"enable_flashinfer_autotune": False},
         profiler_config={
             "profiler": "torch",
-            "torch_profiler_dir": "./vllm_profile",
+            "torch_profiler_dir": f"./vllm_profile/bsz{len(prompts)}/",
         },
+        enable_prefix_caching=False,
+        load_format="dummy",
+        compilation_config={"max_cudagraph_capture_size": 64},
+        speculative_config={"method": "mtp", "num_speculative_tokens": 3},
+        max_num_batched_tokens=32768,
     )
 
+    outputs = llm.generate(prompts, sampling_params)
     llm.start_profile()
 
     # Generate texts from the prompts. The output is a list of RequestOutput
